51d5820a4e9a97375ff9f2d78d6f7111887c1146
Ivan Juras <ijuras@gmail.com>
2017-12-03 14:56:16 +0100

    Add PostCSS import and variable modules

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested')
+    nested = require('postcss-nested'),
+    cssimport = require('postcss-import');
 
 
 gulp.task('default', function () {
@@ -16,7 +17,7 @@ gulp.task('html', function () {
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles/'));
 });
 
