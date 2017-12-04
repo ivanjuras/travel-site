5bce41bfdb5d8eb84e4acc55aba9bfeea667dc74
Ivan Juras <ijuras@gmail.com>
2017-12-04 23:41:46 +0100

    Create mixins and start responsive images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssVars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    cssImport = require('postcss-import');
+    cssImport = require('postcss-import'),
+    cssMixins = require('postcss-mixins');
 
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssMixins, cssVars, nested, autoprefixer]))
             .on('error', function (errorInfo) {
                 console.log(errorInfo.toString());
                 this.emit('end');
